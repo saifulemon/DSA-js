1eba6b0201de25c01899e4b0753194ba0579b22c
saifulemon <463saiful0@gmail.com>
2022-10-30 07:24:40 +0600

    constant and smaller Terms in Big-O-Notation

--- Big-O-Notation/time-of-code.js
+++ Big-O-Notation/time-of-code.js
@@ -1,16 +1,33 @@
-// Time Complexity in same function
-function sumOfAll (n) {
-    let total = 0;
-    for (let i = 0; i <= n; i++) {
-        total += i;
+// // Time Complexity in same function
+// function sumOfAll (n) {
+//     let total = 0;
+//     for (let i = 0; i <= n; i++) {
+//         total += i;
+//     }
+//     return total;
+// }
+// console.log(sumOfAll(10));
+
+
+// // try to different
+// function sumOfAll2 (n) {
+//     return n * n + 1 / 2;
+// }
+// console.log(sumOfAll2(150));
+
+
+// constant and smaller Terms don't metter in big-o
+function min5Log (n) {
+    for (let i = 0; i <= Math.max(5, n); i++) {
+        console.log(i);
     }
-    return total;
 }
-console.log(sumOfAll(10));
+// min5Log(12);  // big-o(n)
 
 
-// try to different
-function sumOfAll2 (n) {
-    return n * n + 1 / 2;
+function max5Log (n) {
+    for (let i = 0; i <= Math.min(5, n); i++) {
+        console.log(i);
+    }
 }
-console.log(sumOfAll2(150));
+max5Log(3);  // big-o(1)
