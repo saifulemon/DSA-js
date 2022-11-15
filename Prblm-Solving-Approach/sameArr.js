2ce7b87e35df25bf278b06577355c18c1202b3ec
saifulemon <463saiful0@gmail.com>
2022-11-15 07:10:14 +0600

    check time complexity & changed the code

--- Prblm-Solving-Approach/sameArr.js
+++ Prblm-Solving-Approach/sameArr.js
@@ -14,3 +14,34 @@ function same(arr1, arr2) {
   return true;
 }
 console.log(same([5, 8, 9], [64, 25, 81]));
+
+
+
+// tyr to different as big O notation
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let sequenceCounter1 = {};
+  let sequenceCounter2 = {};
+
+  for (let val of arr1) {
+    sequenceCounter1[val] = sequenceCounter1[val]++ || 1;
+  }
+  for (let val of arr2) {
+    sequenceCounter2[val] = sequenceCounter2[val]++ || 1;
+  }
+
+  for (let key in sequenceCounter1) {
+    if (!(key **2) in sequenceCounter2) {
+      return false;
+    }
+
+    if(sequenceCounter2[key ** 2] !== sequenceCounter1[key]){
+      return false;
+    }
+  }
+  return true;
+}
+console.log(same([5, 8, 9], [64, 25, 81]));
